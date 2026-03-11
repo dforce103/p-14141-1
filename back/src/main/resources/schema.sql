4d140ae4047aa662091061de965a84583e0e341e
송찬의 <dforce103@gmail.com>
2026-03-11 13:21:33 +0900

    fix: 운영 배포에서 SQL init 비활성화

--- back/src/main/resources/schema.sql
+++ back/src/main/resources/schema.sql
@@ -1,22 +1,6 @@
-DO
-$$
-BEGIN
-    CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-EXCEPTION
-    WHEN OTHERS THEN
-        RAISE NOTICE 'skip pg_stat_statements extension: %', SQLERRM;
-END
-$$;
-
-DO
-$$
-BEGIN
-    CREATE EXTENSION IF NOT EXISTS pgroonga;
-EXCEPTION
-    WHEN OTHERS THEN
-        RAISE NOTICE 'skip pgroonga extension: %', SQLERRM;
-END
-$$;
-
+CREATE
+EXTENSION IF NOT EXISTS pg_stat_statements;
+CREATE
+EXTENSION IF NOT EXISTS pgroonga;
 -- CREATE EXTENSION IF NOT EXISTS postgis;
 -- CREATE EXTENSION IF NOT EXISTS vectorscale CASCADE;
